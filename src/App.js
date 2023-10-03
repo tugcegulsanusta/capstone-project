efdad28d0547d1bfe95298ccfef30f90f9babaf1
Tuğçe Usta <tugce.gulsan@gmail.com>
2023-10-03 10:52:48 +0300

    handle booking

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import Nav from "./components/Nav";
+import Header from "./components/Header";
+import BookingForm from "./components/BookingForm";
+import Main from "./components/Main";
 
 function App() {
   return (
     <>
       <Nav />
+      <Main />
     </>
   );
 }
